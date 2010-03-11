0a6d4c5bc0200a3f4ed82d062613f02805e13178
Bernard Lambeau <blambeau@gmail.com>
2010-03-11 15:58:51 +0100

    Starts the queries for grid presentations.

---
+++ lib/web_stamina/model/install.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'rubyrel'
+require 'rubyrel/commands/command'
+require 'rubyrel/commands/create'
+require 'rubyrel/commands/rel'
+
+handler = 'postgres://stamina:stamina@localhost/stamina'
+def relative_file(file)
+  File.expand_path(File.join(File.dirname(__FILE__), file))
+end
+
+create = Rubyrel::Commands::Create.new
+create.run(__FILE__, ['-h', handler, '--trace', '--verbose', relative_file('stamina.rel')])
+db = Sequel::connect(handler)
+db << File.read(relative_file('stamina.ext'))
+
+rel = Rubyrel::Commands::Rel.new
+rel.run(__FILE__, ['-h', handler, '--trace', '--verbose', '-f', relative_file('stamina.data')])
+rel.run(__FILE__, ['-h', handler, '--trace', '--verbose', '-f', relative_file('test.data')])
