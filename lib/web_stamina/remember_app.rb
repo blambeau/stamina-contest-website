60ad57959f56844381eee16445a94b1ddc7331b2
Bernard Lambeau <blambeau@gmail.com>
2010-03-13 11:42:34 +0100

    Refactors the remember me feature.

--- lib/web_stamina/remember_app.rb
+++ lib/web_stamina/remember_app.rb
@@ -9,21 +9,16 @@ module WebStamina
       @app = app
     end
     
-    # Is a pair (mail, pass) authorized?
-    def authorized_user?(mail, pass)
-      tuple = Waw.resources.sequel_db[:people].filter(:mail => mail, :password => pass, :activation => "").first
-      tuple && (tuple[:admin_level] >= 0)
-    end
-    
     # Manage calls
     def call(env)
       # autolog the user if needed
       remember_me = request.cookies['remember_me']
-      if not(session.logged?) and (remember_me)
-        if not(remember_me.empty?)
-          mail = (Waw.resources.sequel_db[:people].filter(:remember_me => remember_me).first || {})[:mail]
+      if not(session.logged?) and (remember_me) and not(remember_me.empty?)
+        user = Waw.resources.sequel_db[:people].filter(:remember_me => remember_me).first
+        if user
+          session_set(:user, user[:mail]) 
+          Waw.logger.debug("Autologging user #{user[:nickname]}")
         end
-        session_set(:user, mail) 
       end
       
       # make the delegate call now
@@ -31,9 +26,12 @@ module WebStamina
       res = Rack::Response.new(result[2], result[0], result[1])
       
       # Restore the cookie now
-      if session.logged?
-        res.set_cookie('remember_me', session.current_user[:remember_me])
-      else
+      current_user = session.current_user
+      if current_user[:id] and not(remember_me == current_user[:remember_me])
+        Waw.logger.debug("Setting new remember_me cookie for user #{current_user[:nickname]}")
+        res.set_cookie('remember_me', current_user[:remember_me])
+      elsif current_user[:id].nil?
+        Waw.logger.debug("Unsetting remember_me cookie")
         res.delete_cookie('remember_me')
       end
       
